a8eadeee96b2c7694e475ba68e48f38e484139e9
fofananumber1 <fofananumber1@vt.edu>
2026-06-28 01:12:14 -0400

    Task Submission

---
+++ README.md
@@ -0,0 +1,5 @@
+## Interactive Demo for Singing Without a Larynx URA Position ##
+
+This is an interactive demo created as part of the URA position for the Singing Without a Larynx position.
+
+It is a basic audio player which plays one song, Claire de Lune. There is also a volume slider to control volume.
---
+++ page.js
@@ -0,0 +1,38 @@
+const audioContext = new AudioContext();
+
+const audioElement = document.querySelector("audio");
+const track = audioContext.createMediaElementSource(audioElement);
+
+const gainNode = audioContext.createGain();
+const volumeControl = document.getElementById("volumeSlider"); // querySelector(#volumeSlider) did not work
+
+track.connect(audioContext.destination)
+track.connect(gainNode).connect(audioContext.destination);
+
+const playPause = document.querySelector("button");
+
+playPause.addEventListener("click", () => {
+    // Check if context is suspended, if so, resume it
+    if (audioContext.state === "suspended") {
+        audioContext.resume();
+    }
+
+    // Toggle between play and pause
+    if (playPause.dataset.playing === "false") {
+        audioElement.play();
+        playPause.dataset.playing = "true";
+    }
+    else if (playPause.dataset.playing === "true") {
+        audioElement.pause();
+        playPause.dataset.playing = "false";
+    }
+});
+
+audioElement.addEventListener("ended", () => {
+    playPause.dataset.playing = "false";
+});
+
+volumeControl.addEventListener("input", () => {
+    gainNode.gain.value = volumeControl.value;
+});
+
